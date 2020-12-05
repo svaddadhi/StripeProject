a786d34ff4b4232107dd95dafb358c8684756e5d
Vishal Vaddadhi <vishal@techstep.com>
2020-12-04 18:03:33 -0800

    base setup

---
+++ react-ecommerce/src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/header/header';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
