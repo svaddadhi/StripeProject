eaeba33b3bd6d8f65a6252eb71a26a293ecabdff
Vishal Vaddadhi <vishal@techstep.com>
2020-12-08 18:15:07 -0800

    webhook setup stripe checkout fully complete, custom next

---
+++ server/api/webhook.js
@@ -0,0 +1,23 @@
+const stripeAPI = require('../stripe');
+
+const webhook = (req, res) => {
+  const sig = req.headers['stripe-signature'];
+  let event;
+
+  try {
+    event = stripeAPI.webhooks.constructEvent(
+      req['rawBody'],
+      sig,
+      process.env.WEB_HOOK_SECRET
+    );
+  } catch (error) {
+    return res.status(400).send(`WebHook Error ${error}`);
+  }
+
+  if (event.type === 'checkout.session.completed') {
+    const session = event.data.object;
+    console.log('Event data', session);
+  }
+};
+
+module.exports = webhook;
